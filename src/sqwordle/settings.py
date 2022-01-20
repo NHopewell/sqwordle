9ba318ccc7cf5a9e088363d180463def45e16a23
Nick Hopewell <nicholashopewell@gmail.com>
2022-01-19 18:30:07 -0800

    update settings.py

--- src/sqwordle/settings.py
+++ src/sqwordle/settings.py
@@ -1,2 +1,4 @@
+WORDLE_URL = "https://www.powerlanguage.co.uk/wordle/"
 COMMON_WORDS_PATH = "src/sqwordle/utils/5_letter_words.txt"
 SCORED_WORDS_DUMP = "all_scored_words.pickle"
+DRIVER_LOCATION = "src/chromedriver/chromedriver 3"
